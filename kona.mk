ee1ede727ed5dab7bff076f426123329255aa597
mydongistiny <jaysonedson@gmail.com>
2021-12-11 02:31:48 +0200

    sm8250-common: Disable ART Debug for Device builds

--- kona.mk
+++ kona.mk
@@ -299,7 +299,14 @@ PRODUCT_PACKAGES += \
 
 #Power Hal
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+
+# Dex
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+ART_BUILD_TARGET_NDEBUG := true
+ART_BUILD_TARGET_DEBUG := false
+ART_BUILD_HOST_NDEBUG := true
+ART_BUILD_HOST_DEBUG := false
 
 # Speed up
 PRODUCT_DEXPREOPT_SPEED_APPS += \
