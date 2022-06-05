489a7a4ab9d25d1d036f368be4e05592a9c318b5
StarkDroid <gamerprince.exp@gmail.com>
2022-06-05 09:26:08 -0500

    Nuke duplicate flags

--- kona.mk
+++ kona.mk
@@ -127,12 +127,6 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     Phonesky \
     GoogleServicesFramework
 
-# Don't build debug for host or device
-ART_BUILD_TARGET_NDEBUG := true
-ART_BUILD_TARGET_DEBUG := false
-ART_BUILD_HOST_NDEBUG := true
-ART_BUILD_HOST_DEBUG := false
-
 # Dex pre-opt
 WITH_DEXPREOPT := true
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
